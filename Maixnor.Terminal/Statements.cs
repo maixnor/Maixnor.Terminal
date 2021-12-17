5260ce61ec6bf26fae366401734de783e47c226f
maixnor <46966993+maixnor@users.noreply.github.com>
2021-12-17 08:48:47 +0100

    statements now with dictionary help and email statements

---
+++ Maixnor.Terminal/Statements.cs
@@ -0,0 +1,32 @@
+namespace Maixnor.Terminal;
+
+public static class Statements
+{
+    public static string Help => 
+@"
+    No need to worry, you are in a shell.
+    Just hammer away at your keyboard and 
+    find out what the commands do “¯\_(ツ)_/¯“.
+    
+    Here is a list of commands:
+      * help
+      * clear
+      * exit
+      * cv
+      * projects
+      * github
+      * linkedin
+      * email
+";
+
+    public static string Email =>
+@"
+    I have a few emails to choose from:
+    
+    * my personal one:      benjamin@meixner.org
+    * reach me at school:   mei18930@spengergasse.at
+    * my work email:        benjamin.meixner@centauri.at
+
+    Happy emailing ^^.  
+";
+}
