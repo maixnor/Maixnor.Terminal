a90baa8decba492da3ecc87351467d6f5f0506d3
benjamin meixner <mei18930@spengergasse.at>
2022-01-05 22:38:13 +0100

    pwa and easter statements

--- Maixnor.Terminal/Statements.cs
+++ Maixnor.Terminal/Statements.cs
@@ -2,6 +2,25 @@ namespace Maixnor.Terminal;
 
 public static class Statements
 {
+
+    public static string Pwa =>
+@"
+This app you are currently using is a PWA.
+This is short for 'Progressive Web App' which
+means that you can install this application to
+your machine (if you have a chrome-based browser).
+
+Your browser will indicate that the application
+can be installed. The installed version 
+automatically fetches the newest version,
+so no updates or reinstalling necessary.
+
+There are no 'pwa exclusive' features like 
+push notification or file system access in this
+terminal so far.
+
+This PWA has been made using Blazor WASM.
+";
     public static string Help => 
 @"
 No need to worry, you are in a shell.
@@ -13,14 +32,16 @@ Here is a list of commands:
   * help
   * clear
   * projects
+  * pwa
   * source
   * github
   * linkedin
   * email
   * exit
 
-Click anywhere to initiate typing mode and put in some commands.
-There is also an easter egg :).
+Click anywhere to initiate typing mode 
+and put in some commands.
+There is also an 'easter' egg :).
 ";
 
     public static string Email =>
@@ -34,6 +55,25 @@ I have a few emails to choose from:
 Happy emailing ^^.  
 ";
 
+    public static string Easter =>
+@"
+You have not found the easter egg.... yet.
+
+In any case you can find it in the 'source'
+rather easily.
+
+Here are the clues one by one. 
+
+* It has been computed after 
+  seven and a half million years of thought
+
+* It is a rather popular reference to 
+  'The Hitchhiker's Guide to the Galaxy'.
+
+* It may have something to do with the
+  packages installed pacman.
+";
+
     public static string Projects => 
 @"
 I have done a lot during my education and career.
@@ -42,7 +82,7 @@ This includes:
   * The terminal you are currently using 
         shortcut to the code: 'source'
   * A neural network for the MNIST dataset
-  * A Webshop based on the northwind database
+  * A shopfront based on the northwind database
   * A Time Management system for work
   * A Minesweeper App for Android
 
